11e0917b49cc069964bb7e289735c585326e2fa2
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-05-03 08:18:36 +0900

    added comment

--- chapter01/ex06/ex06.c
+++ chapter01/ex06/ex06.c
@@ -5,6 +5,6 @@ int main()
 	int	c;
 
 	while((c = (getchar() != EOF)))
-		printf("%d\n", c); // why this function is called twice once the getchar receive a char?
+		printf("%d\n", c); // why this function is called twice once the getchar receive a char? => becuase getchar recieve a '\n' as well as a char.
 	printf("%d - at EOF\n", c);
 }
