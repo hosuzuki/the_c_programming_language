1dfb7c5aa3f81796213fc04cb1cdfdc1e7411e88
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-05-13 07:26:32 +0900

    little tweek

--- chapter01/ex14/ex14.c
+++ chapter01/ex14/ex14.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define MAXHIST 15
+//#define MAXHIST 15
 #define MAXCHAR 128
 
 int main()
@@ -9,7 +9,7 @@ int main()
 	int	c;
 	int	i;
 	int	len;
-	int	maxvalue;
+//	int	maxvalue;
 	int	cc[MAXCHAR];
 
 	for (i = 0; i < MAXCHAR; ++i)
@@ -17,21 +17,23 @@ int main()
 	while ((c = getchar ()) != EOF)
 		if (c < MAXCHAR)
 			++cc[c];
-	maxvalue = 0;
-	for (i = 1; i < MAXCHAR; ++i)
-		if (cc[i] > maxvalue)
-			maxvalue = cc[i];
-	for (i = 1; i < MAXCHAR; ++i)
+//	maxvalue = 0;
+//	for (i = 1; i < MAXCHAR; ++i)
+//		if (cc[i] > maxvalue)
+//			maxvalue = cc[i];
+	for (i = 0; i < MAXCHAR; ++i)
 	{
 		if (isprint(i))
 			printf("%5d - %c - %5d : ", i, i, cc[i]);
 		else
 			printf("%5d -   - %5d : ", i, cc[i]);
+//		if (cc[i] > 0)
+//		{
+//			if ((len = cc[i] * MAXHIST /maxvalue) <= 0)
+//				len = 1;
+//		}
 		if (cc[i] > 0)
-		{
-			if ((len = cc[i] * MAXHIST /maxvalue) <= 0)
-				len = 1;
-		}
+			len = cc[i];
 		else 
 			len = 0;
 		while (len > 0)
