74158c9aee2032606808b601ac7ad4cd7bb0248b
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-05-24 08:02:33 +0900

    mod

--- chapter01/ex23/ex23.c
+++ chapter01/ex23/ex23.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
-void Ft_remove_cmt(void)
+void ft_remove_cmt(void)
 {
 	int	c;
-	int d;
+	int	d;
 
 	while ((c = getchar()) != EOF)
 	{
 		if (c == '/')
 		{
-			if ((d = getchar()) == '\')
+			if ((d = getchar()) == '/')
 			{
 				while ((c = getchar()) != '\n')
 				{}
+//				putchar('\n');
 			}
 			else
+			{
 				putchar(c);
+				putchar(d);
+			}
 		}
 		else 
 			putchar(c);
 	}
 }
+
+"dfjkjdf \""
 
 int	main(void)
 {
