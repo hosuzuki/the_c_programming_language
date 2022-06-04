6aa2cc73f4c6efcc65a634e81944b1887672c734
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-06-05 08:18:02 +0900

    1st commit

--- chapter02/ex01/ex01_2.c
+++ chapter02/ex01/ex01_2.c
@@ -1,1 +1,25 @@
 //printing appropriate values by direct computation.
+
+#include<stdio.h>
+
+int	main(void)
+{
+	printf("singed char min  = %d\n", ~(~(char)0 >> 1));
+	printf("singed char max  = %d\n", (~(char)0 >> 1));
+
+	printf("singed char min  = %d\n", ~(char)((unsigned char) ~0 >> 1));
+	printf("singed char max  = %d\n", (char)((unsigned char) ~0 >> 1));
+	printf("singed short min = %d\n", ~(short)((unsigned short) ~0 >> 1));
+	printf("singed short max = %d\n", (short)((unsigned short) ~0 >> 1));
+	printf("singed int min   = %d\n", ~(int)((unsigned int) ~0 >> 1));
+	printf("singed int max   = %d\n", (int)((unsigned int) ~0 >> 1));
+	printf("singed long min  = %ld\n", ~(long)((unsigned long) ~0 >> 1));
+	printf("singed long max  = %ld\n", (long)((unsigned long) ~0 >> 1));
+
+	printf("unsinged char max  = %u\n", (unsigned char) ~0);
+	printf("unsinged short max = %u\n", (unsigned short) ~0);
+	printf("unsinged int max   = %u\n", (unsigned int) ~0);
+	printf("unsinged long max  = %lu\n", (unsigned long) ~0);
+	return (0);
+}
+
