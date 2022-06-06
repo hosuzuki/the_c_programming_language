e56517f562d3d99806edb561fe3c853573d70663
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-06-07 08:10:12 +0900

    wrote a code

--- chapter02/ex02/ex02.c
+++ chapter02/ex02/ex02.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+enum loop {NO, YES};
+enum loop okloop = YES;
+
+int main(void)
+{
+	int i;
+	int lim;
+	
+	i = 0;
+	lim = 10;
+	while (okloop = YES)
+	{
+		if (i >= lim - 1)
+			okloop = NO;
+		else if ((c = getchar()) == '\n')
+			okloop = NO;
+		else if (c == EOF)
+			okloop = NO;
+		else 
+		{
+			s[i] = c;
+			++i;
+		}
+	}
+	printf("i: %d\n", i);
+	return (0);
+}
